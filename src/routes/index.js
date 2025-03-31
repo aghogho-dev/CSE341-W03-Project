a0d3c05aedf4abfa45acd4e8bb5c28fcd1f6be1f
Aghogho <monorienaghogho@gmail.com>
2025-03-31 17:19:57 +0100

    Add OAuth 2.0 to root

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,26 @@
 const router = require("express").Router();
+const { auth, requireAuth } = require("express-openid-connect");
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
+
+router.use(auth(config));
 
 router.use("/", require("./swagger"));
 
-router.get("/", (req, res) => res.json({message: "Welcome to CSE 341 Project for Weeks 3 and 4"}));
+router.get("/", (req, res) => {
+    res.json({
+        message: "Welcome to CSE 341 Project for Weeks 3 and 4",
+        authetication: req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out'
+    })
+  }
+);
 
 router.use("/accounts", require("./accounts"));
 
